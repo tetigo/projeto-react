6f028302440fdc4eddfe1ae4569f8f9541686f7d
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2020-02-29 14:40:35 -0300

    server http

---
+++ laboratorios/aula01/server/app-server.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const json = {
+    "Content-Type": "text/html" 
+}
+
+const requisicao = function(request, response){
+    response.writeHead(200, json);
+    response.write("<h1>Bem vindo ao nosso http server</h1>");
+    response.end();
+}
+
+const server = http.createServer(requisicao);
+
+server.listen(3000, function(){
+    console.log('Servidor no ar')
+});
