3069a3b6ecb6fc429b6e96e9469ffc5e0a587097
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2020-02-29 17:31:20 -0300

    inicio projeto 2

---
+++ projeto/server/src/config/server.js
@@ -0,0 +1,18 @@
+// Arquivo responsável por configurar o servidor Node/Express
+const port = 3200;
+
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const server = express(); 
+
+//para toda requisição que chegar, use o bodyparser para
+//interpretar chegadas no formato urlencoded
+server.use(bodyParser.urlencoded({ extended: true }))
+
+//considera o formato json no corpo da requisição
+server.use(bodyParser.json()); 
+
+server.listen(port, function () {
+    console.log(`servidor no ar, na porta ${port}`);
+});
